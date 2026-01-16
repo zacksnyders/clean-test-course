729b888280aebe85b84bd9e107d707aa79d95233
zacksnyders <zack.snyders@gmail.com>
2026-01-16 13:15:34 +0200

    Update Acceptance Tests

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -3,9 +3,22 @@ describe('Shows Appeteasers', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
   it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+    //Navigate to Handhelds.  This makes sure that the link actually exists.
+    cy.contains('Handhelds').click();
+    //Check if Cheese Burger is there.
+    cy.contains('Cheese Burger');
+    //Check if Fajita Tacos is there
+    cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+    //Navigate to Appeteasers.  This makes sure that the link actually exists.
+    cy.contains('Appeteasers').click();
+    //Check if Tater Tots is there.
+    cy.contains('Tater Tots');
+    //Check if Buffalo Wings is there.
+    cy.contains('Buffalo Wings');
+    //Make sure that the Handhelds DO NOT EXIST
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
